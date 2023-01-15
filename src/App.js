d642b2415e6ea254fd5db59d066a14059dedc0ca
saqibashrafff <saqibashraf268@gmail.com>
2023-01-16 00:04:00 +0500

    React Assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [Username, setUsername] = useState("");
+  const [Password, setPassword] = useState("");
+
+  function UsernameChange(event){
+
+    var Uservalue = event.target.value;
+
+    setUsername(Uservalue);
+
+  }
+
+  function PasswordChange(event){
+
+    var PassValue = event.target.value;
+    
+    setPassword(PassValue);
+
+  }
+
+  function login(){
+
+    if (Username == "" || Password == "" ){
+      alert("Please enter your credentials!");
+    }
+    else {
+      alert("Username " + Username + " & Password " + Password);
+
+      setPassword("");
+      setUsername("");
+      
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='LoginSection' >
+      <h1>Login</h1><br></br>
+      <input value={Username} id="fname" type={"text"} placeholder="Username" onChange={UsernameChange}></input><br></br>
+      <input value={Password} type={"password"} placeholder="Password" onChange={PasswordChange} ></input><br></br>
+      <button type={"submit"} onClick={login} >Login</button>
     </div>
   );
 }
 
 export default App;
+
